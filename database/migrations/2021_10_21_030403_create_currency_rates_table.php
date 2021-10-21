b40f026a632043939eea57b9231a749974ef60e9
Feniksdv <feniksdv@gmail.com>
2021-10-21 06:25:15 +0300

    Создал две модели и миграции к ним. Поправил ссылку и отображение в файле Readme

---
+++ database/migrations/2021_10_21_030403_create_currency_rates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrencyRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency_rates', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->char('from ', 3);
+            $table->char('to ', 3);
+            $table->integer('rate ');
+            $table->float('multiplier')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency_rates');
+    }
+}
